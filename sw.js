2cca1728dd89e553b0cf1f22305ef1b10c8b846e
jasonfiammetta <jason.fiammetta@gmail.com>
2020-09-07 16:52:52 -0400

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-42117b49f3c4950492c6.js"
+    "url": "webpack-runtime-00a8798f4f0bae2bdd1a.js"
   },
   {
     "url": "framework-fd2b85530eec85104a39.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "4523841d123c1926e46f5c9762ff204e"
+    "revision": "8a07f61849dc1c4f56933de18e1d2221"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-0097d26fbd474b34ff9b.js"
